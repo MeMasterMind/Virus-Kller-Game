125e6fb34ce9906aba09b302fb0d3ad49572bd09
arpantaneja <64348757+arpantaneja@users.noreply.github.com>
2020-05-17 19:14:53 +0530

    Update game.js

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ function preload() {
     game.load.image('weapon', 'https://ik.imagekit.io/arpantaneja/arrow_1D7V7vgBM.png');
     game.load.image('virus', 'https://ik.imagekit.io/arpantaneja/JavaScript/Covid-19_Game/coronavirus_PNG39_DSbl8UXttu.png');
     game.load.image('spawn', 'https://ik.imagekit.io/arpantaneja/spawn_safety2_udFKg54ET2.png');
+    game.load.image('background', 'https://ik.imagekit.io/arpantaneja/debug-grid-1920x1920_eT69sGYec.png');
 }
 
 var sprite;
@@ -28,6 +29,8 @@ function create() {
 
 
 
+    game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    game.world.setBounds(0, 0, 1920, 1920);
 
     bullets = game.add.group();
     bullets.enableBody = true;
@@ -36,8 +39,11 @@ function create() {
     bullets.createMultiple(50, 'bullet');
     bullets.setAll('checkWorldBounds', true);
     bullets.setAll('outOfBoundsKill', true);
+    
+    game.physics.startSystem(Phaser.Physics.P2JS)
 
-    game.stage.backgroundColor = '#00ffe1'
+    
+    
 
 
    virus = game.add.sprite(game.world.randomX, game.world.randomY, 'virus');
@@ -96,16 +102,20 @@ function create() {
 
 
 
+
     spawn = game.add.sprite(600, 200, 'spawn');
     spawn.anchor.set(0, 5);
 
 
     sprite = game.add.sprite(700, 300, 'weapon');
+    game.physics.p2.enable(sprite);
+    sprite.body.fixedRotation = true;
+    game.camera.follow(sprite, Phaser.Camera.FOLLOW_LOCKON, 10.1, 10.1);    
+    sprite.fixedToCamera = true;
+    sprite.cameraOffset.setTo(500, 100);
 
 
-    sprite.anchor.set(0.5);
-
-    game.physics.arcade.enable([sprite, bullets, virus, spawn]);
+    game.physics.arcade.enable([bullets, virus, spawn]);
     game.physics.arcade.enable(virus.children);
 
     spawn.body.drag.set(0);
@@ -113,15 +123,14 @@ function create() {
 
     sprite.body.collideWorldBounds = true;
 
-    sprite.body.drag.set(0);
-
-    sprite.body.maxVelocity.set(800);
 
 
+    sprite.anchor.set(0.5);
 
 
-    cursors = this.input.keyboard.createCursorKeys();
 
+
+    cursors = game.input.keyboard.createCursorKeys();
     fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
 
 
@@ -132,9 +141,10 @@ function update() {
 
 
 
-   sprite.rotation = game.physics.arcade.angleToPointer(sprite);
+  sprite.rotation = game.physics.arcade.angleToPointer(sprite);
 
-// This is where I got the problem
+
+
 
    game.physics.arcade.collide(virus.children, bullets, (child, bullet) => {
 
@@ -144,37 +154,28 @@ function update() {
 
    });
 
+   
     if (cursors.up.isDown)
     {
-        sprite.body.velocity.y = -300;
+        game.camera.y -= 4;
     }
-
     else if (cursors.down.isDown)
-    {   sprite.body.velocity.y = 300;
-
-    }
-
-    else
     {
-        sprite.body.velocity.y = 0;
+        game.camera.y += 4;
     }
-
-
-
 
     if (cursors.left.isDown)
     {
-        sprite.body.velocity.x = -300;
+        game.camera.x -= 4;
     }
     else if (cursors.right.isDown)
     {
-        sprite.body.velocity.x = 300;
-    }
-    else
-    {
-        sprite.body.velocity.x = 0;
+        game.camera.x += 4;
     }
 
+
+
+    
     if (game.input.activePointer.isDown)
     {
         fire();
@@ -193,13 +194,13 @@ function update() {
 }
 
 function render() {
-    var debug = this.game.debug;
+   /* var debug = this.game.debug;
 
     debug.physicsGroup(this.world, 'rgba(0,0,255,0.25)');
 
     virus.children.forEach(function (child) {
         debug.body(child, 'rgba(255,0,0,0.25)');
-    });
+    });*/
 }
 
 
